c225527a43fe32cd04f1272974f384027c9e6f5f
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-09-13 11:03:57 +0700

    feat: calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 "use client";
 
-import { authClient, useSession } from "~/lib/auth-client";
+import { CalendarDemo } from "./_components/calendar-demo";
 
 const HomePage = () => {
-  const { data } = useSession();
-  const { data: activeMember } = authClient.useActiveMember();
-  const { data: activeOrganization } = authClient.useActiveOrganization();
-  const { data: listOrganizations } = authClient.useListOrganizations();
-
   return (
     <div>
-      <div>HomePage</div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      <pre>{JSON.stringify(activeMember, null, 2)}</pre>
-      <pre>{JSON.stringify(activeOrganization, null, 2)}</pre>
-      <pre>{JSON.stringify(listOrganizations, null, 2)}</pre>
+      <CalendarDemo />
     </div>
   );
 };
