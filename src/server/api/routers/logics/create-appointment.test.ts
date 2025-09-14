f39564f34c44735fdef1c500057ffdf8f0a663ee
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2025-09-14 15:59:25 +0700

    feat: test overlapping booking of room or doctor

--- src/server/api/routers/logics/create-appointment.test.ts
+++ src/server/api/routers/logics/create-appointment.test.ts
@@ -3,6 +3,7 @@ import { afterEach, expect, it } from "bun:test";
 import { db } from "~/server/db";
 import { createAppointment } from "./create-appointment";
 
+const timezone = "UTC";
 const startAt = new Date("0001-01-01T09:00:00Z");
 const endAt = new Date("0001-01-01T09:30:00Z");
 
@@ -34,8 +35,8 @@ async function setupSuccessfulInput() {
 afterEach(async () => {
   await db.appointment.deleteMany({
     where: {
-      startAt: { gte: startAt },
-      endAt: { lte: endAt },
+      startAt: { gte: new Date("0001-01-01T00:00:00Z") },
+      endAt: { lte: new Date("0001-12-31T23:59:59Z") },
     },
   });
 });
@@ -44,6 +45,7 @@ it("should create appointment successfully", async () => {
   const input = await setupSuccessfulInput();
 
   const appointment = await createAppointment({
+    timezone,
     prisma: db,
     input: input,
   });
@@ -81,6 +83,7 @@ it("should throw an error if the room is already booked for the same time", asyn
   const input = await setupSuccessfulInput();
 
   const appointment1 = await createAppointment({
+    timezone,
     prisma: db,
     input: input,
   });
@@ -89,6 +92,7 @@ it("should throw an error if the room is already booked for the same time", asyn
 
   try {
     await createAppointment({
+      timezone,
       prisma: db,
       input: input,
     });
@@ -117,14 +121,115 @@ it("should throw an error if the room is already booked for the same time", asyn
   expect(createdAppointmentsCount).toBe(1);
 });
 
+it("should throw an error if the room is already booked with overlapping time #1", async () => {
+  const input = await setupSuccessfulInput();
+
+  const appointment1 = await createAppointment({
+    timezone,
+    prisma: db,
+    input: {
+      ...input,
+      startAt: new Date("0001-01-01T09:00:00Z"),
+      endAt: new Date("0001-01-01T09:30:00Z"),
+    },
+  });
+
+  expect(appointment1).toBeDefined();
+
+  try {
+    await createAppointment({
+      timezone,
+      prisma: db,
+      input: {
+        ...input,
+        startAt: new Date("0001-01-01T08:00:00Z"),
+        endAt: new Date("0001-01-01T10:00:00Z"),
+      },
+    });
+
+    expect(false, "Should not create appointment").toBeTruthy();
+  } catch (error) {
+    expect(error).toBeDefined();
+
+    expect(error instanceof TRPCError).toBeTruthy();
+
+    const trpcError = error as TRPCError;
+
+    expect(trpcError.code).toBe("CONFLICT");
+
+    expect(trpcError.message).toBe("Room is already booked for this time");
+  }
+
+  const createdAppointmentsCount = await db.appointment.count({
+    where: {
+      roomId: input.roomId,
+      startAt: { gte: new Date("0001-01-01T09:00:00Z") },
+      endAt: { lte: new Date("0001-01-01T09:30:00Z") },
+    },
+  });
+
+  expect(createdAppointmentsCount).toBe(1);
+});
+
+it("should throw an error if the room is already booked with overlapping time #2", async () => {
+  const input = await setupSuccessfulInput();
+
+  const appointment1 = await createAppointment({
+    timezone,
+    prisma: db,
+    input: {
+      ...input,
+      startAt: new Date("0001-01-01T08:00:00Z"),
+      endAt: new Date("0001-01-01T10:00:00Z"),
+    },
+  });
+
+  expect(appointment1).toBeDefined();
+
+  try {
+    await createAppointment({
+      timezone,
+      prisma: db,
+      input: {
+        ...input,
+        startAt: new Date("0001-01-01T09:00:00Z"),
+        endAt: new Date("0001-01-01T09:30:00Z"),
+      },
+    });
+
+    expect(false, "Should not create appointment").toBeTruthy();
+  } catch (error) {
+    expect(error).toBeDefined();
+
+    expect(error instanceof TRPCError).toBeTruthy();
+
+    const trpcError = error as TRPCError;
+
+    expect(trpcError.code).toBe("CONFLICT");
+
+    expect(trpcError.message).toBe("Room is already booked for this time");
+  }
+
+  const createdAppointmentsCount = await db.appointment.count({
+    where: {
+      roomId: input.roomId,
+      startAt: { gte: new Date("0001-01-01T08:00:00Z") },
+      endAt: { lte: new Date("0001-01-01T10:00:00Z") },
+    },
+  });
+
+  expect(createdAppointmentsCount).toBe(1);
+});
+
 it("should withstand thundering herd of create appointment requests into the same room and time slots at the same time", async () => {
   const input = await setupSuccessfulInput();
 
-  const concurrent = 1_000;
+  const concurrent = 100;
 
   const appointments = await Promise.allSettled(
     Array.from({ length: concurrent }, async () => {
       return await createAppointment({
+        timezone,
         prisma: db,
         input: input,
       });
@@ -136,6 +241,222 @@ it("should withstand thundering herd of create appointment requests into the sam
       startAt: { gte: input.startAt },
       endAt: { lte: input.endAt },
       roomId: input.roomId,
+    },
+  });
+
+  const success = appointments.filter((x) => x.status === "fulfilled").length;
+  const failed = appointments.filter((x) => x.status === "rejected").length;
+
+  expect(success).toBe(1);
+  expect(failed).toBe(concurrent - 1);
+  expect(createdAppointmentsCount).toBe(1);
+});
+
+it("should throw an error if the doctor is already booked for the same time", async () => {
+  const input = await setupSuccessfulInput();
+
+  const appointment1 = await createAppointment({
+    timezone,
+    prisma: db,
+    input: input,
+  });
+
+  expect(appointment1).toBeDefined();
+
+  // Create a second appointment with different room but same doctor and time
+  const [differentRoomId] = await Promise.all([
+    db.room
+      .findMany()
+      .then((rooms) => rooms.find((room) => room.id !== input.roomId)?.id),
+  ]);
+
+  expect(differentRoomId).toBeDefined();
+
+  try {
+    await createAppointment({
+      timezone,
+      prisma: db,
+      input: {
+        ...input,
+        roomId: differentRoomId!,
+      },
+    });
+
+    expect(false, "Should not create appointment").toBeTruthy();
+  } catch (error) {
+    expect(error).toBeDefined();
+
+    expect(error instanceof TRPCError).toBeTruthy();
+
+    const trpcError = error as TRPCError;
+
+    expect(trpcError.code).toBe("CONFLICT");
+
+    expect(trpcError.message).toBe("Doctor is already booked for this time");
+  }
+
+  const createdAppointmentsCount = await db.appointment.count({
+    where: {
+      startAt: { gte: input.startAt },
+      endAt: { lte: input.endAt },
+      doctorId: input.doctorId,
+    },
+  });
+
+  expect(createdAppointmentsCount).toBe(1);
+});
+
+it("should throw an error if the doctor is already booked with overlapping time #1", async () => {
+  const input = await setupSuccessfulInput();
+
+  const appointment1 = await createAppointment({
+    timezone,
+    prisma: db,
+    input: {
+      ...input,
+      startAt: new Date("0001-01-01T09:00:00Z"),
+      endAt: new Date("0001-01-01T09:30:00Z"),
+    },
+  });
+
+  expect(appointment1).toBeDefined();
+
+  // Create a second appointment with different room but same doctor and overlapping time
+  const [differentRoomId] = await Promise.all([
+    db.room
+      .findMany()
+      .then((rooms) => rooms.find((room) => room.id !== input.roomId)?.id),
+  ]);
+
+  expect(differentRoomId).toBeDefined();
+
+  try {
+    await createAppointment({
+      timezone,
+      prisma: db,
+      input: {
+        ...input,
+        roomId: differentRoomId!,
+        startAt: new Date("0001-01-01T08:00:00Z"),
+        endAt: new Date("0001-01-01T10:00:00Z"),
+      },
+    });
+
+    expect(false, "Should not create appointment").toBeTruthy();
+  } catch (error) {
+    expect(error).toBeDefined();
+
+    expect(error instanceof TRPCError).toBeTruthy();
+
+    const trpcError = error as TRPCError;
+
+    expect(trpcError.code).toBe("CONFLICT");
+
+    expect(trpcError.message).toBe("Doctor is already booked for this time");
+  }
+
+  const createdAppointmentsCount = await db.appointment.count({
+    where: {
+      doctorId: input.doctorId,
+      startAt: { gte: new Date("0001-01-01T09:00:00Z") },
+      endAt: { lte: new Date("0001-01-01T09:30:00Z") },
+    },
+  });
+
+  expect(createdAppointmentsCount).toBe(1);
+});
+
+it("should throw an error if the doctor is already booked with overlapping time #2", async () => {
+  const input = await setupSuccessfulInput();
+
+  const appointment1 = await createAppointment({
+    timezone,
+    prisma: db,
+    input: {
+      ...input,
+      startAt: new Date("0001-01-01T08:00:00Z"),
+      endAt: new Date("0001-01-01T10:00:00Z"),
+    },
+  });
+
+  expect(appointment1).toBeDefined();
+
+  // Create a second appointment with different room but same doctor and overlapping time
+  const [differentRoomId] = await Promise.all([
+    db.room
+      .findMany()
+      .then((rooms) => rooms.find((room) => room.id !== input.roomId)?.id),
+  ]);
+
+  expect(differentRoomId).toBeDefined();
+
+  try {
+    await createAppointment({
+      timezone,
+      prisma: db,
+      input: {
+        ...input,
+        roomId: differentRoomId!,
+        startAt: new Date("0001-01-01T09:00:00Z"),
+        endAt: new Date("0001-01-01T09:30:00Z"),
+      },
+    });
+
+    expect(false, "Should not create appointment").toBeTruthy();
+  } catch (error) {
+    expect(error).toBeDefined();
+
+    expect(error instanceof TRPCError).toBeTruthy();
+
+    const trpcError = error as TRPCError;
+
+    expect(trpcError.code).toBe("CONFLICT");
+
+    expect(trpcError.message).toBe("Doctor is already booked for this time");
+  }
+
+  const createdAppointmentsCount = await db.appointment.count({
+    where: {
+      doctorId: input.doctorId,
+      startAt: { gte: new Date("0001-01-01T08:00:00Z") },
+      endAt: { lte: new Date("0001-01-01T10:00:00Z") },
+    },
+  });
+
+  expect(createdAppointmentsCount).toBe(1);
+});
+
+it("should withstand thundering herd of create appointment requests for the same doctor and time slots at the same time", async () => {
+  const input = await setupSuccessfulInput();
+
+  const concurrent = 100;
+
+  const availableRooms = await db.room.findMany();
+
+  const appointments = await Promise.allSettled(
+    Array.from({ length: concurrent }, async (_, index) => {
+      // Use different rooms for each concurrent request
+      const differentRoomId = availableRooms[index % availableRooms.length]?.id;
+      if (!differentRoomId) {
+        throw new Error("No available rooms found");
+      }
+
+      return await createAppointment({
+        timezone,
+        prisma: db,
+        input: {
+          ...input,
+          roomId: differentRoomId,
+        },
+      });
+    }),
+  );
+
+  const createdAppointmentsCount = await db.appointment.count({
+    where: {
+      startAt: { gte: input.startAt },
+      endAt: { lte: input.endAt },
+      doctorId: input.doctorId,
     },
   });
 
